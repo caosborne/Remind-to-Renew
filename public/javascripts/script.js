8c168455fe947a436686719f9b01233e0e75f476
Christopher Osborne <chris.a.osborne@gmail.com>
2016-09-16 15:57:05 -0500

    gitignore changes

---
+++ public/javascripts/script.js
@@ -0,0 +1,25 @@
+$( function() {
+    $( "#datepicker" ).datepicker({
+      changeMonth: true,
+      changeYear: true
+    });
+    $( "#datepicker2" ).datepicker({
+      changeMonth: true,
+      changeYear: true
+    });
+    $( "#datepicker3" ).datepicker({
+      changeMonth: true,
+      changeYear: true
+    });
+
+    // emailjs.init("user_uSErnimTl9oTYeNiuuicB");
+    //
+    // // parameters: service_id, template_id, template_parameters
+    // emailjs.send("default_service","remind2renew",{name: "James", notes: "Check this out!"});
+
+
+      // $('#datetimepicker1').datetimepicker();
+      // $('#datetimepicker2').datetimepicker();
+      // $('#datetimepicker3').datetimepicker();
+  }
+);
